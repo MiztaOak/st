b314f00cf5d58f426fe7db7a5e9b3e4a6d281e33
Johan Ek <johan.ek@tuta.com>
2026-05-19 00:34:09 +0200

    Updated colorscheme to better fit gruvbox light mode

--- config.h
+++ config.h
@@ -111,24 +111,24 @@ float alpha = 0.8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
   /* 8 normal colors */
-	[0] = "#fbf1c7", /* hard contrast: #f9f5d7 / soft contrast: #f2e5bc */
-	[1] = "#cc241d", /* red     */
-	[2] = "#98971a", /* green   */
-	[3] = "#d79921", /* yellow  */
-	[4] = "#458588", /* blue    */
-	[5] = "#b16286", /* magenta */
-	[6] = "#689d6a", /* cyan    */
-	[7] = "#7c6f64", /* white   */
+	[0] = "#928374", /* hard contrast: #f9f5d7 / soft contrast: #f2e5bc */
+	[1] = "#9d0006", /* red     */
+	[2] = "#79740e", /* green   */
+	[3] = "#b57614", /* yellow  */
+	[4] = "#076678", /* blue    */
+	[5] = "#8f3f71", /* magenta */
+	[6] = "#427b58", /* cyan    */
+	[7] = "#3c3836", /* white   */
 
 	/* 8 bright colors */
-	[8]  = "#928374", /* black   */
-	[9]  = "#9d0006", /* red     */
-	[10] = "#79740e", /* green   */
-	[11] = "#b57614", /* yellow  */
-	[12] = "#076678", /* blue    */
-	[13] = "#8f3f71", /* magenta */
-	[14] = "#427b58", /* cyan    */
-	[15] = "#3c3836", /* white   */
+	[8]  = "#fbf1c7", /* black   */
+	[9]  = "#cc241d", /* red     */
+	[10] = "#98971a", /* green   */
+	[11] = "#d79921", /* yellow  */
+	[12] = "#458588", /* blue    */
+	[13] = "#b16286", /* magenta */
+	[14] = "#689d61", /* cyan    */
+	[15] = "#7c6f64", /* white   */
 
 	[255] = 0,
 
